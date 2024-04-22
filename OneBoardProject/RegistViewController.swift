80408c62aaf351dc342e7c508918eb1e193c3c8b
mkwoong <mgu6636@gmail.com>
2024-04-22 19:28:33 +0900

    [regist]TextField 기능구현

--- OneBoardProject/RegistViewController.swift
+++ OneBoardProject/RegistViewController.swift
@@ -9,21 +9,31 @@ import UIKit
 
 class RegistViewController: UIViewController {
 
+    @IBOutlet weak var registNmaeTextField: UITextField!
+    @IBOutlet weak var registIDTextField: UITextField!
+    @IBOutlet weak var registPasswordTextField: UITextField!
+    
+    func registTextFieldSetUp() {
+        // TextField 라운드 처리
+        registNmaeTextField.borderStyle = .roundedRect
+        registIDTextField.borderStyle = .roundedRect
+        registPasswordTextField.borderStyle = .roundedRect
+        
+        // TextField 클리어 기능
+        registNmaeTextField.clearButtonMode = .always
+        registIDTextField.clearButtonMode = .always
+        registPasswordTextField.clearButtonMode = .always
+    }
+    
+    // 화면 터치 시 TextField 키보드 내려가는 기능
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+             self.view.endEditing(true)
+       }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        registTextFieldSetUp()
 
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
