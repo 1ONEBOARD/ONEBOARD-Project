099b378008d8ca79bfc161df91996e4a8412ce7c
mkwoong <mgu6636@gmail.com>
2024-04-22 20:48:19 +0900

    [regist]입력값에따른 버튼 색변경기능구현

--- OneBoardProject/RegistViewController.swift
+++ OneBoardProject/RegistViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import CoreData
 
-class RegistViewController: UIViewController {
+class RegistViewController: UIViewController, UITextFieldDelegate {
+    
+    // PersistentContainer에 접근
+    var persistentContainer: NSPersistentContainer? {
+        (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+    }
 
     @IBOutlet weak var registNmaeTextField: UITextField!
     @IBOutlet weak var registIDTextField: UITextField!
     @IBOutlet weak var registPasswordTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
     
     func registTextFieldSetUp() {
         // TextField 라운드 처리
@@ -33,7 +40,37 @@ class RegistViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         registTextFieldSetUp()
-
+        
+        // 회원가입 버튼 라운드처리
+        self.registerButton.layer.masksToBounds = true
+        self.registerButton.layer.cornerRadius = 6
+        
+        self.registNmaeTextField.delegate = self
+        self.registIDTextField.delegate = self
+        self.registPasswordTextField.delegate = self
+        
+        // TextField 입력값 변경 감지
+        self.registNmaeTextField.addTarget(self, action: #selector(self.TextFieldDidChanged(_:)), for: .editingChanged)
+        self.registIDTextField.addTarget(self, action: #selector(self.TextFieldDidChanged(_:)), for: .editingChanged)
+        self.registPasswordTextField.addTarget(self, action: #selector(self.TextFieldDidChanged(_:)), for: .editingChanged)
+    }
+    
+    // TextField 입력값 변하면 유효성 검사
+    @objc func TextFieldDidChanged(_ sender: UITextField) {
+        // 3개의 TextField가 채워졌는지 확인
+        if !(self.registNmaeTextField.text?.isEmpty ?? true) && !(self.registIDTextField.text?.isEmpty ?? true) && !(self.registPasswordTextField.text?.isEmpty ?? true) {
+            updateRegisterButton(willActive: true)
+        } else {
+            updateRegisterButton(willActive: false)
+        }
+    }
+    
+    func updateRegisterButton(willActive: Bool) {
+        if (willActive == true) {
+            self.registerButton.backgroundColor = UIColor(named: "GcooColor")
+        } else {
+            self.registerButton.backgroundColor = UIColor(named: "defaultsColor")
+        }
     }
 
 }
