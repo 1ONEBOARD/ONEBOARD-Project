1d9d2183587c06ff6476545ea74505443182678b
wxxd-fxrest <fxrest704@gmail.com>
2024-04-23 14:17:48 +0900

    [Login] ID password matching check(false 일 경우 error label show), password hidden/show(eye icon button)

--- OneBoardProject/OneBoardProject/LoginPageController.swift
+++ OneBoardProject/OneBoardProject/LoginPageController.swift
@@ -2,30 +2,106 @@
 //  LoginPageController.swift
 //  OneBoardProject
 //
-
 import UIKit
 
-class LoginPageController: UIViewController {
+class LoginPageController: UIViewController, UITextFieldDelegate {
 
+    // MARK: - User Info
+    struct UserInfo {
+        var userName: String
+        var id: String
+        var password: String
+    }
+    
+    let users: [UserInfo] = [
+         UserInfo(userName: "user1", id: "userid1", password: "userpw1"),
+         UserInfo(userName: "user2", id: "userid2", password: "userpw2"),
+         UserInfo(userName: "user3", id: "userid2", password: "userpw3")
+    ]
+    
+    
+    // MARK: - Outlets
+    @IBOutlet weak var IDTextField: UITextField!
+    @IBOutlet weak var PWTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var errorAlertLabel: UILabel!
+    @IBOutlet weak var loginButtonTopConstraint: NSLayoutConstraint!
     
+    
+    // MARK: - Properties
+    var showPasswordButton = UIButton(type: .custom)
+
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        errorAlertLabel.isHidden = true
+        
+        IDTextField.delegate = self
+        PWTextField.delegate = self
+        
+        PWTextField.isSecureTextEntry = true
+        PWTextField.textContentType = .password
+        
+        IDTextField.keyboardType = .asciiCapable
+        IDTextField.clearButtonMode = .always
+
+        IDTextField.placeholder = "ID"
+        PWTextField.placeholder = "password"
+        
+
+        showPasswordButton.frame = CGRect(x: 0, y: 0, width: 30, height: 20)
+        showPasswordButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        showPasswordButton.addTarget(self, action: #selector(togglePasswordVisibility), for: .touchUpInside)
+        showPasswordButton.tintColor = UIColor(named: "defaultsColor")
+
+        let buttonContainer = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 20))
+        buttonContainer.addSubview(showPasswordButton)
+
+        PWTextField.rightView = buttonContainer
+        PWTextField.rightViewMode = .always
     }
     
+    
+    // MARK: - Button Actions
     @IBAction func loginButtonTapped(_ sender: UIButton) {
-        let targetStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        
-        let targetVC = targetStoryboard.instantiateViewController(withIdentifier: "MainPage")
-        
-        if targetVC != nil {
-            targetVC.modalPresentationStyle = .fullScreen
-            present(targetVC, animated: true, completion: nil)
-        } else {
-            print("Failed to instantiate view controller with identifier 'MainPage'")
+        view.endEditing(true)
+
+        guard let userID = IDTextField.text, !userID.isEmpty else {
+            print("Please enter a user ID")
+            showErrorAlert(message: "아이디를 입력해 주세요.")
+            return
         }
+        
+        guard let password = PWTextField.text, !password.isEmpty else {
+            print("Please enter a password")
+            showErrorAlert(message: "비밀번호를 입력해 주세요.")
+            return
+        }
+        
+        if let matchedUserInfo = users.first(where: { $0.id == userID && $0.password == password }) {
+            print("Login successful: \(matchedUserInfo.userName)")
+            let targetStoryboard = UIStoryboard(name: "Main", bundle: nil)
+
+            let targetVC = targetStoryboard.instantiateViewController(withIdentifier: "MainPage")
+
+            if targetVC != nil {
+                targetVC.modalPresentationStyle = .fullScreen
+                present(targetVC, animated: true, completion: nil)
+            } else {
+                print("화면 전환 실패, login -> main")
+            }
+        } else {
+            print("아이디 또는 비밀번호가 일치하지 않습니다")
+            showErrorAlert(message: "아이디 또는 비밀번호가 일치하지 않습니다. 다시 확인해 주세요.")
+        }
+        
+        print("userID: \(userID)")
+        print("password: \(password)")
     }
+
 
     @IBAction func signUpButtonTapped(_ sender: UIButton) {
         guard let signUpVC = storyboard?.instantiateViewController(withIdentifier: "SignUpPage") else {
@@ -34,5 +110,36 @@ class LoginPageController: UIViewController {
         
         signUpVC.modalPresentationStyle = .fullScreen
         present(signUpVC, animated: true, completion: nil)
+    }
+    
+    @objc func togglePasswordVisibility() {
+        PWTextField.isSecureTextEntry.toggle()
+        let imageName = PWTextField.isSecureTextEntry ? "eye.slash" : "eye"
+        showPasswordButton.setImage(UIImage(systemName: imageName), for: .normal)
+    }
+    
+    
+    // MARK: - Helper Methods
+    func showErrorAlert(message: String) {
+        errorAlertLabel.text = message
+        UIView.animate(withDuration: 0.3) {
+            self.errorAlertLabel.isHidden = false
+            self.errorAlertLabel.alpha = 1.0
+            self.loginButtonTopConstraint.constant = 30
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    
+    // MARK: - UITextFieldDelegate
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.placeholder = ""
+
+        UIView.animate(withDuration: 0.3) {
+            self.errorAlertLabel.isHidden = true
+            self.errorAlertLabel.alpha = 0.3
+            self.loginButtonTopConstraint.constant = 16
+            self.view.layoutIfNeeded()
+        }
     }
 }
