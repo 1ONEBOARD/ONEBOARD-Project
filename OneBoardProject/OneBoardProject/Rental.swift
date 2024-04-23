2122851c121de76f45421c78f5950492830d2fd6
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-04-23 11:49:26 +0900

    [user] feat UserTableView

---
+++ OneBoardProject/OneBoardProject/Rental.swift
@@ -0,0 +1,16 @@
+//
+//  Rental.swift
+//  OneBoardProject
+//
+//  Created by 배지해 on 4/23/24.
+//
+
+import Foundation
+
+struct Rental {
+    var kickBoardID: String
+    var kickBoardNumber: Int
+    var rentalStartTime: String
+    var rentalTotalTime: Int
+    var rentalPrice: Int
+}
