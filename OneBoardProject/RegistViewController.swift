3247a8172d8887c80bc8fbf1951d1c5922aa65a5
mkwoong <mgu6636@gmail.com>
2024-04-23 18:13:12 +0900

    [regist]회원가입버튼알림추가

--- OneBoardProject/RegistViewController.swift
+++ OneBoardProject/RegistViewController.swift
@@ -57,6 +57,16 @@ class RegistViewController: UIViewController {
         checkPasswordTextField.text = ""
     }
     
+    func resetLabelForm() {
+        nameInformLabel.isHidden = true
+        idInformLabel.text = "영문, 숫자를 포함한 8-16자"
+        idInformLabel.textColor = UIColor(named: "defaultsColor")
+        passwordInformLabel.text = "영문, 숫자를 포함한 8-16자"
+        passwordInformLabel.textColor = UIColor(named: "defaultsColor")
+        passwordCheckInformLabel.isHidden = true
+    }
+    
+    
     //가리기 버튼
     
     @IBOutlet weak var passwordEyeButton: UIButton!
@@ -75,7 +85,28 @@ class RegistViewController: UIViewController {
     
     
     // TextField 유효성 검사
+    
     @IBAction func nameChanged(_ sender: Any) {
+        if let name = registNmaeTextField.text {
+            if let errorMessage = invalidName(name) {
+                nameInformLabel.isHidden = false
+                nameInformLabel.text = errorMessage
+                nameInformLabel.textColor = .red
+            }
+            else {
+                nameInformLabel.text = "환영합니다."
+                nameInformLabel.textColor = UIColor(named: "GcooColor")
+            }
+        }
+    }
+    
+    func invalidName(_ value: String) -> String? {
+        let reqularExpression = "^.*([가-힣])+.*$"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
+        if !predicate.evaluate(with: value) {
+            return "알맞지 않은 형식입니다."
+        }
+        return nil
         
     }
     
@@ -93,7 +124,7 @@ class RegistViewController: UIViewController {
     }
     
     func invalidID(_ value: String) -> String? {
-        let reqularExpression = "^(?=.*[A-Za-z])(?=.*[0-9]).{8,16}"
+        let reqularExpression = "(?=.*[a-z])(?=.*[\\d]).{8,16}"
         let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
         if !predicate.evaluate(with: value) {
             return "알맞지 않은 형식입니다."
@@ -116,13 +147,33 @@ class RegistViewController: UIViewController {
     }
     
     func invalidPassword(_ value: String) -> String? {
-        let reqularExpression2 = "^(?=.*[A-Za-z])(?=.*[0-9]).{8,16}"
-        let predicate2 = NSPredicate(format: "SELF MATCHES %@", reqularExpression2)
-        if !predicate2.evaluate(with: value) {
+        let reqularExpression = "(?=.*[a-z])(?=.*[\\d]).{8,16}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
+        if !predicate.evaluate(with: value) {
             return "알맞지 않은 형식입니다"
         }
         return nil
     }
+    
+    // 회원가입 버튼 기능
+    
+    @IBAction func addUserInfomButton(_ sender: Any) {
+        let alert = UIAlertController(title: "회원가입 하시겠습니까?", message: "입력하신 정보로 회원가입이 진행됩니다.", preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "예", style: .default, handler: { [self]_ in
+            resetForm()
+            resetLabelForm()
+        })
+        let cancel = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+        
+        alert.addAction(confirm)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
     
 }
 
@@ -156,8 +207,8 @@ extension RegistViewController: UITextFieldDelegate {
     
     // 화면 터치 시 TextField 키보드 내려가는 기능
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
-             self.view.endEditing(true)
-       }
+        self.view.endEditing(true)
+    }
     
     // 비밀번호 확인
     func isSamePassword(_ first: UITextField, _ second: UITextField) -> Bool {
@@ -177,7 +228,14 @@ extension RegistViewController: UITextFieldDelegate {
     // TextField 입력값 변하면 유효성 검사
     @objc func TextFieldDidChanged(_ sender: UITextField) {
         // 4개의 TextField가 채워졌는지 확인 & 비밀번호 일치성 여부확인
-        if !(self.registNmaeTextField.text?.isEmpty ?? true) && !(self.registIDTextField.text?.isEmpty ?? true) && !(self.registPasswordTextField.text?.isEmpty ?? true) && !(self.checkPasswordTextField.text?.isEmpty ?? true) && isSamePassword(registPasswordTextField, checkPasswordTextField) {
+        if !(self.registNmaeTextField.text?.isEmpty ?? true) &&
+            !(self.registIDTextField.text?.isEmpty ?? true) &&
+            !(self.registPasswordTextField.text?.isEmpty ?? true) &&
+            !(self.checkPasswordTextField.text?.isEmpty ?? true) &&
+            isSamePassword(registPasswordTextField, checkPasswordTextField) &&
+            (idInformLabel.text == "사용 가능한 아이디입니다.") &&
+            (passwordInformLabel.text == "사용 가능한 비밀번호입니다.") &&
+            (nameInformLabel.text == "환영합니다.") {
             updateRegisterButton(willActive: true)
         } else {
             updateRegisterButton(willActive: false)
@@ -194,7 +252,7 @@ extension RegistViewController: UITextFieldDelegate {
         }
     }
     
-
+    
 }
 
 
